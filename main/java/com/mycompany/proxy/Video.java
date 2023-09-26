d21aa574db2597112e5f90102d96311d75de5fd9
joelnune <91698993+joelnune@users.noreply.github.com>
2023-09-25 21:31:18 -0300

    first commit

---
+++ main/java/com/mycompany/proxy/Video.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proxy;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+package refactoring_guru.proxy.example.some_cool_media_library;
+
+public class Video {
+    public String id;
+    public String title;
+    public String data;
+
+    Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.data = "Random video.";
+    }
+}
---
+++ main/java/com/mycompany/proxy/YouTubeCacheProxy.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proxy;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+import refactoring_guru.proxy.example.some_cool_media_library.ThirdPartyYouTubeClass;
+import refactoring_guru.proxy.example.some_cool_media_library.ThirdPartyYouTubeLib;
+import refactoring_guru.proxy.example.some_cool_media_library.Video;
+
+import java.util.HashMap;
+
+public class YouTubeCacheProxy implements ThirdPartyYouTubeLib {
+    private ThirdPartyYouTubeLib youtubeService;
+    private HashMap<String, Video> cachePopular = new HashMap<String, Video>();
+    private HashMap<String, Video> cacheAll = new HashMap<String, Video>();
+
+    public YouTubeCacheProxy() {
+        this.youtubeService = new ThirdPartyYouTubeClass();
+    }
+
+    @Override
+    public HashMap<String, Video> popularVideos() {
+        if (cachePopular.isEmpty()) {
+            cachePopular = youtubeService.popularVideos();
+        } else {
+            System.out.println("Retrieved list from cache.");
+        }
+        return cachePopular;
+    }
+
+    @Override
+    public Video getVideo(String videoId) {
+        Video video = cacheAll.get(videoId);
+        if (video == null) {
+            video = youtubeService.getVideo(videoId);
+            cacheAll.put(videoId, video);
+        } else {
+            System.out.println("Retrieved video '" + videoId + "' from cache.");
+        }
+        return video;
+    }
+
+    public void reset() {
+        cachePopular.clear();
+        cacheAll.clear();
+    }
+}
